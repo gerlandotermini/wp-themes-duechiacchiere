7e27cc24aabdae8194f8b1021bb13d44439be515
Gerlando Termini <gerlando@termini.family>
2022-06-06 09:45:53 -0400

    Added rel=noopener noreferrer, per https://newbedev.com/link-with-target-blank-and-rel-noopener-noreferrer-still-vulnerable

--- js/duechiacchiere.js
+++ js/duechiacchiere.js
@@ -60,6 +60,7 @@ document.querySelector( '#mobile-menu-trigger' ).addEventListener( 'click', func
 document.querySelectorAll( 'a' ).forEach( link => {
   if ( link.getAttribute( 'href' ) && link.hostname !== location.hostname ) {
     link.target = '_blank';
+    link.rel = 'noopener noreferrer';
   }
 });
 
