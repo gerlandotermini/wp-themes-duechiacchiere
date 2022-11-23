b4762144efd7cb7e6db03685209f739867039ca9
Gerlando Termini <gerlando@termini.family>
2022-11-23 04:46:41 +0100

    Added logic to check if email address in comment is valid - take 2

--- dev/assets/js/script.js
+++ dev/assets/js/script.js
@@ -158,7 +158,7 @@ window.addEventListener( 'DOMContentLoaded', ( event ) => {
         submitForm = submitForm && ( node.value.trim() != '' );
       } );
 
-      if ( e.target.querySelector( '#email' ).value.toLowerCase().match( /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ) == null ) {
+      if ( e.target.querySelector( '#email' ) && e.target.querySelector( '#email' ).value.toLowerCase().match( /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ) == null ) {
         submitForm = false;
       }
 
