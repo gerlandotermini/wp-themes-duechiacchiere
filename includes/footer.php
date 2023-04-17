1192c15a6bde6f8da5a24c9356acd408ab0f95c2
Gerlando Termini <gerlando@termini.family>
2023-04-17 15:20:34 +0200

    Updated footer links, tweaked footer styles

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,18 @@
+	<?php
+	// Random post
+	$random_post = new WP_Query( array ( 'orderby' => 'rand', 'posts_per_page' => '1' ) );
+	$random_post_url = '/';
+	if ( !empty( $random_post->posts ) ) {
+		$random_post_url = get_permalink( $random_post->posts[ 0 ]->ID );
+	}
+	wp_reset_postdata();
+	?>
+
 	<footer>
 		<div class="about-me">
 			<h2>Due Chiacchiere</h2>
 			<p>
-				L'arte di scrivere sta nel saper tirar fuori da quel nulla che si &egrave; capito della vita tutto il resto. Poi, finita la pagina, ti accorgi che quel che avevi capito &egrave; proprio un nulla.
+				Tra vent'anni sarai pi&ugrave; dispiaciuto per le cose che non hai fatto che per quelle che hai fatto. Quindi sciogli gli ormeggi, naviga lontano dal porto sicuro. Cattura i venti delle opportunità nelle tue vele.
 			</p>
 		</div>
 
@@ -12,13 +22,15 @@
 				<li><a href="https://supporthost.com/it/a340" title="Il fornitore di hosting che ospita queste pagine">Il servizio che mi ospita</a></li>
 				<li><a href="https://it.wordpress.org/" title="Sito italiano del sistema WordPress per la gestione dei contenuti">Il sistema di gestione</a></li>
 				<li><a href="https://github.com/gerlandotermini/wp-themes-duechiacchiere" title="L'archivio dove scaricare una copia del mio tema" hreflang="en">Il vestito del blog</a></li>
-				<li><a href="https://www.rxstrip.it/" title="Sito del disegnatore che ha donato un corpo virtuale a camu">L'artista del ragazzo</a></li>
+				<li><a href="https://akismet.com/" title="Sito del plugin per WordPress per filtrare lo spam nei commenti" hreflang="en">Il guardiano dei commenti</a></li>
+				<li><a href="https://www.rxstrip.it/" title="Sito del disegnatore che ha donato un corpo virtuale a camu">Il disegnatore di camu</a></li>
 			</ul>
 		</nav>
 
 		<nav aria-labelledby="useful-links">
 			<h2 id="useful-links">Collegamenti utili</h2>
 			<ul>
+				<li><a href="/chi-sono">Breve storia del tenutario</a></li>
 				<li><a href="/licenza" rel="nofollow" title="Pagina in italiano che descrive la licenza per i contenuti">La licenza di attribuzione</a></li>
 				<li><a href="/accessibile" title="Leggi le informazioni sul livello di accessibilit&agrave; di queste pagine">Un sito accessibile a tutti</a></li>
 				<li><a href="/moderazione" title="Domande e risposte sulle mie regole di moderazione dei commenti">Le regole per i commenti</a></li>
@@ -30,25 +42,14 @@
 		<nav aria-labelledby="misc-links">
 			<h2 id="misc-links">Varie ed eventuali</h2>
 			<ul>
-				<li><a href="/chi-sono">Ti presento l'inquilino</a></li>
 				<li><a href="/contatto">Lascia un messaggio</a></li>
-				<li><a href="/feed">Il <span lang="en">feed</span> <abbr title="really simple syndication" lang="en">RSS</abbr> degli articoli</a></li>
+				<li><a href="/feed">Il <span lang="en">feed</span> degli articoli</a></li>
+				<li><a href="/comments/feed">Il <span lang="en">feed</span> dei commenti</a></li>
 				<li><a href="/feed/scrissi-oggi">Il <span lang="en">feed</span> di oggi nel passato</a></li>
-				
-				
+				<li><a href="<?= $random_post_url ?>">Leggi un post a caso</a></li>
 			</ul>
 		</nav>
 	</footer>
-
-	<?php
-	// Random post
-	$random_post = new WP_Query( array ( 'orderby' => 'rand', 'posts_per_page' => '1' ) );
-	$random_post_url = '/';
-	if ( !empty( $random_post->posts ) ) {
-		$random_post_url = get_permalink( $random_post->posts[ 0 ]->ID );
-	}
-	wp_reset_postdata();
-	?>
 
 	<nav aria-labelledby="mobile-toolbar-label">
 		<h2 id="mobile-toolbar-label" class="visually-hidden">Navigazione mobile</h2>
