e69003b6adb2cbc56a0c2ea2837c5238da0ce7fc
Gerlando Termini <gerlando@termini.family>
2023-05-10 23:41:19 +0200

    Fixed a minor accessibility issue with the Read More link

--- index.php
+++ index.php
@@ -72,7 +72,7 @@
 					<?php endif // is post ?>
 				</header>
 				<div class="entry">
-					<?php the_content( '<span class="visually-hidden">' . the_title( '', '', false ) . ': </span>Leggi il resto &raquo;', false ); ?>
+					<?php the_content( 'Leggi il resto <span aria-hidden="true">&raquo;</span> <span class="visually-hidden">di ' . the_title( '', '', false ) . '</span>', false ); ?>
 				</div>
 			</article>
 
