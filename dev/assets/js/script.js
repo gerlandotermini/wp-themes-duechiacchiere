54b89e3a72b8ca24233d7d83128b685e45e28123
Gerlando Termini <gerlando@termini.family>
2023-05-10 23:51:07 +0200

    Tweaked text appended to external links for screen readers

--- dev/assets/js/script.js
+++ dev/assets/js/script.js
@@ -347,7 +347,7 @@ window.addEventListener( 'DOMContentLoaded', ( event ) => {
       link.target = '_blank';
 
       if ( !link.querySelector( '.visually-hidden' ) ) {
-        link.insertAdjacentHTML( 'beforeend', '<span class="visually-hidden"> (apri link in una nuova finestra)</span>');
+        link.insertAdjacentHTML( 'beforeend', '<span class="visually-hidden"> (apre una nuova finestra)</span>');
       }
 
       // See https://codersblock.com/blog/external-links-new-tabs-and-accessibility/
