3bf481fd4c63442f976922f3bc2c657e31bf1759
Gerlando Termini <gerlando@termini.family>
2024-03-17 01:44:50 +0100

    Increased 'naked' width of other fields

--- includes/comments.php
+++ includes/comments.php
@@ -37,9 +37,9 @@ $comment_count = get_comments_number();
 		'comment_notes_after' => '',
 		'comment_field' => '<p class="comment-form-comment"><label for="comment" class="visually-hidden">Commento</label><textarea id="comment" name="comment" cols="60" rows="8" maxlength="65525" required="required"></textarea></p>',
 		'fields' => array(
-			'author' => '<p class="comment-form-author"><label for="author">Nome <span class="required">*</span></label> <input id="author" name="author" type="text" value="" size="5" maxlength="245" required="required"></p>',
-			'email' => '<p class="comment-form-email"><label for="email">Email <span class="required">*</span></label> <input id="email" name="email" type="text" value="" size="5" maxlength="100" required="required"></p>',
-			'url' => '<p class="comment-form-url"><label for="url">Sito Web</label> <input id="url" name="url" type="text" value="" size="5" maxlength="200"></p>',
+			'author' => '<p class="comment-form-author"><label for="author">Nome <span class="required">*</span></label> <input id="author" name="author" type="text" value="" size="25" maxlength="245" required="required"></p>',
+			'email' => '<p class="comment-form-email"><label for="email">Email <span class="required">*</span></label> <input id="email" name="email" type="text" value="" size="25" maxlength="100" required="required"></p>',
+			'url' => '<p class="comment-form-url"><label for="url">Sito Web</label> <input id="url" name="url" type="text" value="" size="25" maxlength="200"></p>',
 			'cookies' => '<input type="hidden" value="1" name="wp-comment-cookies-consent">'
 		),
 		'format' => 'html5',
