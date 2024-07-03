00657b14c47149ae58420f39d3ad478b6c36ee51
Gerlando Termini <gerlando@termini.family>
2024-07-03 16:40:45 +0200

    Added comment count ONLY for the administrator, not for visitors

--- index.php
+++ index.php
@@ -36,20 +36,6 @@
 				$categories_html[] = '<a href="' . get_category_link( $a_category->term_id ). '">' . $a_category->name . '</a>';
 			}
 			$categories_html = implode( ', ', $categories_html );
-
-			// $comment_count = get_comments_number();
- 
-			// switch( $comment_count ) {
-			// 	case 0:
-			// 		// $comments_html = '<a class="comments-link" href="' . get_permalink() . '#comment" aria-label="Esprimi la tua opinione su '. $GLOBALS[ 'post' ]->post_title . '">Lascia un commento</a>';
-			// 		$comments_html = '';
-			// 		break;
-			// 	case 1:
-			// 		$comments_html = '<a class="comments-link" href="' . get_permalink() . '#comments" aria-label="Leggi il commento per '. $GLOBALS[ 'post' ]->post_title . '">1 commento</a><span class="visually-hidden"> &mdash; </span><a class="skip-inline" href="' . get_permalink() . '#comment" aria-label="Esprimi la tua opinione su '. $GLOBALS[ 'post' ]->post_title . '">Lascia un commento</a>';
-			// 		break;
-			// 	default:
-			// 		$comments_html = '<a class="comments-link" href="' . get_permalink() . '#comments" aria-label="Leggi i ' . $comment_count . ' commenti per ' . $GLOBALS[ 'post' ]->post_title . '">' . $comment_count . ' commenti</a><span class="visually-hidden"> &mdash; </span><a class="skip-inline" href="' . get_permalink() . '#comment" aria-label="Esprimi la tua opinione su '. $GLOBALS[ 'post' ]->post_title . '">Lascia un commento</a>';
-			// }
 			?>
 
 			<article <?php if ( $GLOBALS[ 'wp_query' ]->current_post == $GLOBALS[ 'wp_query' ]->post_count - 1 ) { echo ' class="last"'; } ?>>
@@ -59,12 +45,12 @@
 					<p class="post-meta">
 						<span class="visually-hidden">Scritto il giorno </span><time datetime="<?php the_time( 'Y-m-d H:i:s' ) ?>"><?= strtolower( get_the_time('j F Y') ); ?></time>
 						<?php
+							if ( is_user_logged_in() ) {
+								echo '[' . get_comments_number() . '] ';
+							}
 							if ( !empty( $categories_html ) ) {
 								echo '<span class="visually-hidden">, archiviato</span> in ' . $categories_html;
 							}
-							// if ( !empty( $comments_html ) ) {
-							// 	echo ' <span class="comment-separator">&mdash;</span> ' . $comments_html;
-							// }
 							if ( !is_single() ) {
 								edit_post_link( '[M]', ' ' );
 							}
