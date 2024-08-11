c7759a0d33e0e83253f9f9adcc15922feb4f5b78
Gerlando Termini <gerlando@termini.family>
2024-08-11 17:44:00 +0200

    I had forgotten to apply the new styles to the contact form

--- includes/contact-form.php
+++ includes/contact-form.php
@@ -65,7 +65,7 @@ include_once( 'header.php' ) ?>
 
 <div id="main-wrapper">
 	<main id="content">
-		<article>
+		<article id="comments">
 			<header>
 				<h1><?php the_title( '', '' ) ?></h1>
 			</header>
