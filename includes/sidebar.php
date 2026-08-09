759fd84334a39be38fd1725a92864dba6e534c09
Gerlando Termini <gerlando@termini.family>
2026-08-09 20:53:49 +0200

    Darkened august link color to meet accessibility guidelines

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,7 +3,7 @@
 		<h2 class="visually-hidden">Cerca nel sito</h2>
 		<form role="search" action="<?= esc_url( home_url( '/' ) ); ?>" method="get" id="search-form">
 			<label for="search-field" class="visually-hidden">Digita le parole da cercare e premi invio</label>
-			<input type="search" id="search-field" aria-controls="live-results" aria-expanded="false" aria-autocomplete="list" autocomplete="off" name="s" required placeholder="Cerca nell'archivio...">
+			<input type="search" id="search-field" role="combobox" aria-controls="live-results" aria-expanded="false" aria-autocomplete="list" autocomplete="off" name="s" required="" placeholder="Cerca nell'archivio..." aria-owns="live-results">
 			<button type='submit' id="search-button" class="svg" aria-label="Avvia la ricerca">Cerca</button>
 		</form>
 		<ul id="live-results" role="listbox" aria-live="polite" aria-label="Risultati della ricerca"></ul>
